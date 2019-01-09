a20637202364524c1da96bdad0a6454547a53e61
Harith Khairolanuar <33217725+harithkhairol@users.noreply.github.com>
2019-01-09 13:58:00 +0800

    Update fb-config.php

--- fb-config.php
+++ fb-config.php
@@ -5,8 +5,8 @@ session_start();
 require_once "Facebook/autoload.php";
 
 $FB = new \Facebook\Facebook([
-	'app_id' => '510733956086232',
-	'app_secret' => '7d4328fe0845c03349c4a3460a08285c',
+	'app_id' => 'YOUR APP ID',
+	'app_secret' => 'YOUR APP SECRET',
 	'default_graph version' => 'v2.10'
 	]);
 
